305ac330b460faf49f10845d62ac269ae7fc4d89
washio takahiro <wasiotw@yahoo.co.jp>
2018-06-06 12:29:51 +0000

    課題2

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,4 @@
 class Blog < ApplicationRecord
     validates :title, presence: true
     validates :title, length: { in: 1..140 }
-    root to: 'blogs#index'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :blogs do
     collection do
       post :confirm
+      root to: 'blogs#index'
     end
   end
 end
