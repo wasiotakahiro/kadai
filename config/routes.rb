9bfe064fac14623f0dca5b482b30996963e4fba1
鷲尾貴弘 <washiotakahiro@washiotakahironoMacBook-ea.local>
2018-06-25 17:20:43 +0900

    課題2”

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'tops/index'
+  resources :sessions, only: [:new, :create, :destroy]
+    resources :users
   resources :blogs do
     collection do
       post :confirm
