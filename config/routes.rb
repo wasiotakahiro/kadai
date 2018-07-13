7e86d8ac6c720ad721aea0c8fa3dda93f4eb9096
wasiotakahiro <wasiotw@yahoo.co.jp>
2018-07-14 07:03:19 +0900

    mailer2”

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  resources :contacts
   resources :sessions, only: [:new, :create, :destroy]
   resources :favorites, only: [:create, :destroy]
     resources :users
+    mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   resources :blogs do
     collection do
       post :confirm
